5f70a5efadd76bae2d6da7a772decdd4ebe8582a
Garrett Smith <g@rre.tt>
2017-08-22 11:07:27 -0500

    Enhance task loop support
    Includes ability to start early and to control whether cb is
    called on stop (defaults to True)

--- guild/task_support.py
+++ guild/task_support.py
@@ -8,24 +8,36 @@ class Stop(Exception):
 def series_timestamp():
     return int(time.time() * 1000)
 
-def loop(cb, interval, stop):
-    cb_fun, cb_args = cb
+def loop(cb, interval, stop, first=None, on_stop=True):
+    try:
+        _loop(cb, interval, stop, first, on_stop)
+    except Stop:
+        pass
+    except KeyboardInterrupt:
+        pass
+
+def _loop(cb, interval, stop, first, on_stop):
+    loop_interval = first if first is not None else interval
     start = time.time()
     while True:
-        sleep = _sleep_interval(interval, start)
-        try:
-            stop_signal = stop.poll(sleep)
-        except KeyboardInterrupt:
-            break
+        sleep = _sleep_interval(loop_interval, start)
+        loop_interval = interval
+        stop_signal = stop.poll(sleep)
         if stop_signal:
+            if on_stop:
+                _loop_cb(cb)
             stop.send("ack")
             break
-        try:
-            cb_fun(*cb_args)
-        except Stop:
-            break
+        else:
+            _loop_cb(cb)
+
+def _loop_cb(cb):
+    cb_fun, cb_args = cb
+    cb_fun(*cb_args)
 
 def _sleep_interval(interval, start):
+    if interval <= 0:
+        return 0
     now_ms = int(time.time() * 1000)
     interval_ms = int(interval * 1000)
     start_ms = int(start * 1000)
