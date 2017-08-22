5a9acc9fa75fa25c4a170a62cefbd894fc0a60da
Garrett Smith <g@rre.tt>
2017-08-22 16:12:57 -0500

    Fix bug in guild version

--- guild/devmode.py
+++ guild/devmode.py
@@ -6,9 +6,10 @@ import guild.app
 def maybe_set_git_commit():
     dot_git = os.path.join(guild.app.home(), ".git")
     if os.path.exists(dot_git):
-        guild.__git_commit__ = _git_commit()
+        guild.__git_commit__ = _git_commit(dot_git)
 
-def _git_commit():
-    cmd = "git log -1 --oneline | cut -d' ' -f1"
+def _git_commit(dot_git):
+    cmd = ("git -C \"%s\" log -1 --oneline | cut -d' ' -f1"
+           % dot_git)
     raw = subprocess.check_output(cmd, shell=True)
     return raw.strip()
