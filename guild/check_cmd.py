6bdf662bad18b52b948a07ef8907462b184d95a8
Garrett Smith <g@rre.tt>
2017-08-29 12:19:13 -0500

    Hide stderr by default and provide --verbose option to enable
    TF logs stuff on import to stderr that we don't want to show by
    default when checking versions.

--- guild/check_cmd.py
+++ guild/check_cmd.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -19,13 +20,17 @@ def add_parser(subparsers):
         metavar="TEST",
         dest="tests",
         action="append")
+    p.add_argument(
+        "-v", "--verbose",
+        help="show check details",
+        action="store_true")
     p.set_defaults(func=main)
 
 def main(args):
     if args.all_tests or args.tests:
         _run_tests(args)
     else:
-        _print_info()
+        _print_info(args)
 
 def _run_tests(args):
     import guild.test
@@ -40,10 +45,10 @@ def _run_tests(args):
     if not success:
         guild.cli.error()
 
-def _print_info():
+def _print_info(args):
     _print_guild_info()
     _print_python_info()
-    _print_tensorflow_info()
+    _print_tensorflow_info(args.verbose)
     _print_nvidia_tools_info()
     _print_werkzeug_info()
     _print_psutil_info()
@@ -58,16 +63,17 @@ def _print_python_info():
 def _python_version():
     return sys.version.replace("\n", "")
 
-def _print_tensorflow_info():
+def _print_tensorflow_info(verbose):
     # Run externally to avoid tf logging to our stderr
-    _print_check_results("tensorflow-check")
+    _print_check_results("tensorflow-check", verbose)
 
 def _print_nvidia_tools_info():
     _print_check_results("nvidia-tools-check")
 
-def _print_check_results(script_name):
+def _print_check_results(script_name, verbose=False):
     script_path = guild.app.script(script_name)
-    out = subprocess.check_output(script_path)
+    stderr = None if verbose else open(os.devnull, "w")
+    out = subprocess.check_output(script_path, stderr=stderr)
     sys.stdout.write(out.decode(sys.stdout.encoding))
 
 def _print_werkzeug_info():
