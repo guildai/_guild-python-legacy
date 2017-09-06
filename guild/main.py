f4af56725fb2faa5b96bc834b53d632b178d4ab7
Garrett Smith <g@rre.tt>
2017-09-06 14:47:33 -0500

    Use our own py binary rather than Bazel's; drop guild-lite

--- guild/main.py
+++ guild/main.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
 
-import guild.main_impl
+import guild
+import guild.app
+import guild.cli
 
 import guild.check_cmd
 import guild.evaluate_cmd
@@ -12,8 +14,9 @@ import guild.runs_cmd
 import guild.train_cmd
 import guild.view_cmd
 
-if __name__ == "__main__":
-    guild.main_impl.main([
+def main():
+    _set_git_commit()
+    guild.cli.main([
         guild.check_cmd,
         guild.evaluate_cmd,
         guild.prepare_cmd,
@@ -23,3 +26,28 @@ if __name__ == "__main__":
         guild.train_cmd,
         guild.view_cmd,
     ])
+
+def _set_git_commit():
+    guild.__git_commit__ = _git_commit()
+
+def _git_commit():
+    src_home = _find_source_home()
+    if src_home:
+        cmd = ("git -C \"%s\" log -1 --oneline | cut -d' ' -f1" % src_home)
+        raw = subprocess.check_output(cmd, shell=True)
+        return raw.strip()
+    else:
+        return None
+
+def _find_source_home():
+    cur = guild.app.home()
+    while True:
+        if cur == "/" or cur == "":
+            break
+        if os.path.exists(os.path.join(cur, ".git")):
+            return cur
+        cur = os.path.dirname(cur)
+    return None
+
+if __name__ == "__main__":
+    main()
