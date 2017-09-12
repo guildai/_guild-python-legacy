8e54554a6a315909bc90e60f79c5371e80de71cf
Garrett Smith <g@rre.tt>
2017-09-12 16:51:51 -0500

    Option --files to query

--- guild/query_cmd.py
+++ guild/query_cmd.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import guild.cli
 import guild.cmd_support
 # Avoid expensive imports here
 
@@ -22,14 +23,29 @@ def add_parser(subparsers):
         "--series",
         help="print series keys available for the run",
         action="store_true")
+    p.add_argument(
+        "--files",
+        help="print files associated with the run",
+        action="store_true")
     p.set_defaults(func=main)
 
+OPTIONS = ["series", "files"]
+
 def main(args):
+    verify_zero_or_one_options(args)
     run = guild.cmd_support.run_for_args(args)
     if args.series:
         _print_series(run)
+    elif args.files:
+        _print_files(run)
     else:
         _print_header(run)
+
+def verify_zero_or_one_options(args):
+    count = sum([getattr(args, opt) for opt in OPTIONS])
+    if count > 1:
+        options_desc = ", ".join([("--%s" % opt) for opt in OPTIONS])
+        guild.cli.error("provide at most one option: %s" % options_desc)
 
 def _print_header(run):
     import guild.op_util
@@ -41,8 +57,14 @@ def _print_header(run):
 
 def _print_series(run):
     import guild.db
-
     db = guild.db.init_for_opdir(run.opdir)
     for key in db.series_keys():
         sys.stdout.write(key)
         sys.stdout.write("\n")
+
+def _print_files(run):
+    cwd = os.path.abspath(".")
+    for root, dirs, files in os.walk(run.opdir):
+        for f in files:
+            path = os.path.join(root, f)
+            print(os.path.relpath(path, cwd))
